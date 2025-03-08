1c40e11bbbecbab21f1485008c079b4213664efc
Asaduzzaman04 <asadchy152@gmail.com>
2025-03-09 01:14:20 +0600

    redesign teams card

--- src/components/about-us/AboutTeams.jsx
+++ src/components/about-us/AboutTeams.jsx
@@ -12,6 +12,10 @@ import Sanchari from '../../assets/images/Teams/Shakti_Karmakar_nexGenix.png';
 import Tibra from '../../assets/images/Teams/Tibra_paul_nexgenix.png';
 import asad from '../../assets/images/Teams/Asaduzzaman_nexgenix.png';
 import Riaz from '../../assets/images/Teams/Riaz_uddin_ngenix.png';
+import Afsana from '../../assets/images/Teams/Afsana_Tasnim_Labonno.png';
+import Anyat from '../../assets/images/Teams/Anayet_hossen_Masud_nexgenix.png';
+import Jamshadul from '../../assets/images/Teams/Mohammad_Jamshadul_Islam_Minhaj_nexgenix.png';
+import urmi from '../../assets/images/Teams/Urmi_Karmakar_nexgenix.png';
 
 const AboutTeams = () => {
   const [selectedMember, setSelectedMember] = useState(null);
@@ -35,7 +39,8 @@ const AboutTeams = () => {
       position: 'Operational and Innovation Strategy (Intern)',
       department: 'Strategy & Innovation',
       company: 'NexGenix',
-      bio: "Walid is an innovative thinker and strategist at NexGenix, focusing on operational efficiency and business growth. His expertise in market research and process optimization helps drive new ideas and innovation for our company's success."
+      bio: "Walid is an innovative thinker and strategist at NexGenix, focusing on operational efficiency and business growth. His expertise in market research and process optimization helps drive new ideas and innovation for our company's success.",
+      category: 'Team Leader'
     },
     {
       id: 3,
@@ -44,7 +49,8 @@ const AboutTeams = () => {
       position: 'Front-end Developer and Quality Assurance (Intern)',
       department: 'Development & QA',
       company: 'NexGenix',
-      bio: 'Sohel is a skilled front-end developer and quality assurance expert at NexGenix. With a passion for creating seamless user experiences, he ensures our websites and applications are both visually appealing and highly functional.'
+      bio: 'Sohel is a skilled front-end developer and quality assurance expert at NexGenix. With a passion for creating seamless user experiences, he ensures our websites and applications are both visually appealing and highly functional.',
+      category: 'Team Leader'
     },
     {
       id: 4,
@@ -53,7 +59,8 @@ const AboutTeams = () => {
       position: 'UI/UX Designer',
       department: 'Design',
       company: 'NexGenix',
-      bio: 'Supta is a creative UI/UX designer dedicated to crafting user-centric designs at NexGenix. She specializes in wireframing, prototyping, and enhancing user experiences to make digital interactions intuitive and engaging.'
+      bio: 'Supta is a creative UI/UX designer dedicated to crafting user-centric designs at NexGenix. She specializes in wireframing, prototyping, and enhancing user experiences to make digital interactions intuitive and engaging.',
+      category: 'Team Leader'
     },
     {
       id: 5,
@@ -62,7 +69,8 @@ const AboutTeams = () => {
       position: 'UI/UX Designer (Intern)',
       department: 'Design',
       company: 'NexGenix',
-      bio: 'Pinki is a UI/UX design intern passionate about creating visually compelling and user-friendly designs. She works closely with the design team to optimize digital interfaces and improve user engagement.'
+      bio: 'Pinki is a UI/UX design intern passionate about creating visually compelling and user-friendly designs. She works closely with the design team to optimize digital interfaces and improve user engagement.',
+      category: 'Team Member'
     },
     {
       id: 6,
@@ -71,7 +79,8 @@ const AboutTeams = () => {
       position: 'Junior Marketing Executive',
       department: 'Marketing & Strategy',
       company: 'NexGenix',
-      bio: 'Riaz Uddin is a results-driven marketing executive focused on brand growth, campaign strategy, and audience engagement at NexGenix.'
+      bio: 'Riaz Uddin is a results-driven marketing executive focused on brand growth, campaign strategy, and audience engagement at NexGenix.',
+      category: 'Team Member'
     },
     {
       id: 7,
@@ -80,7 +89,8 @@ const AboutTeams = () => {
       position: 'Back-end Developer (Intern)',
       department: 'Development',
       company: 'NexGenix',
-      bio: 'Tibra is a back-end development intern at NexGenix, specializing in server-side logic, databases, and API development. He plays a key role in building scalable and efficient back-end solutions for our digital products.'
+      bio: 'Tibra is a back-end development intern at NexGenix, specializing in server-side logic, databases, and API development. He plays a key role in building scalable and efficient back-end solutions for our digital products.',
+      category: 'Team Member'
     },
     {
       id: 8,
@@ -89,7 +99,8 @@ const AboutTeams = () => {
       position: 'Graphic Designer (Intern)',
       department: 'Design',
       company: 'NexGenix',
-      bio: 'Tonmoy is a creative graphic design intern at NexGenix, specializing in branding, visual storytelling, and digital graphics. His innovative designs help strengthen our brand identity and marketing campaigns.'
+      bio: 'Tonmoy is a creative graphic design intern at NexGenix, specializing in branding, visual storytelling, and digital graphics. His innovative designs help strengthen our brand identity and marketing campaigns.',
+      category: 'Team Member'
     },
     {
       id: 9,
@@ -98,25 +109,71 @@ const AboutTeams = () => {
       position: 'Copywriting & Client Strategy Execution (Intern)',
       department: 'Marketing & Strategy',
       company: 'NexGenix',
-      bio: 'Sanchari is a talented copywriter and strategist at NexGenix, crafting compelling content that enhances brand messaging. Her expertise in content strategy and storytelling ensures effective engagement with target audiences.'
+      bio: 'Sanchari is a talented copywriter and strategist at NexGenix, crafting compelling content that enhances brand messaging. Her expertise in content strategy and storytelling ensures effective engagement with target audiences.',
+      category: 'Team Member'
     },
     {
       id: 10,
+      name: 'Urmi Karmaker',
+      image: urmi,
+      position: 'Head of Operations',
+      department: 'Operations',
+      company: 'NexGenix',
+      bio: 'Urmi Karmaker is an operations expert at NexGenix, managing day-to-day operations and ensuring efficiency in all aspects of the business. Her focus is on process improvement and operational excellence.',
+      category: 'Team Leader'
+    },
+
+    {
+      id: 11,
+      name: 'Afsana Tasnim Laboni',
+      image: Afsana,
+      position: 'Project Manager',
+      department: 'Project Management',
+      company: 'NexGenix',
+      bio: 'Afsana Tasnim Laboni is a dynamic project manager who leads cross-functional teams to deliver successful projects at NexGenix. She excels in communication, planning, and ensuring projects are executed on time.',
+      category: 'Team Leader'
+    },
+    {
+      id: 12,
+      name: 'Anyat Hossain',
+      image: Anyat,
+      position: 'Head of Marketing',
+      department: 'Marketing',
+      company: 'NexGenix',
+      bio: 'Anyat Hossain is a strategic marketing expert with a strong focus on digital campaigns, brand building, and market growth. He leads the marketing team at NexGenix, driving impactful campaigns and results.',
+      category: 'Team Leader'
+    },
+    {
+      id: 13,
+      name: 'Mohammed Jamshadul Islam',
+      image: Jamshadul,
+      position: 'Lead Developer',
+      department: 'Development',
+      company: 'NexGenix',
+      bio: 'Mohammed Jamshadul Islam is a highly skilled lead developer at NexGenix, specializing in software architecture and development practices. He guides the development team to ensure high-quality code and scalable solutions.',
+      category: 'Team Leader'
+    },
+    {
+      id: 15,
       name: 'Asaduzzaman',
       image: asad,
       position: 'Full Stack Developer (Intern)',
       department: 'Development',
       company: 'NexGenix',
-      bio: 'Asaduzzaman is a full-stack developer intern at NexGenix with expertise in front-end and back-end technologies. He is passionate about building dynamic web applications, ensuring seamless performance and user experience.'
+      bio: 'Asaduzzaman is a full-stack developer intern at NexGenix with expertise in front-end and back-end technologies. He is passionate about building dynamic web applications, ensuring seamless performance and user experience.',
+      category: 'Team Leader'
     }
   ];
 
   // Get CEO data
   const ceoData = teamMembers.find((member) => member.position === 'CEO');
 
+  const teamLeader = teamMembers.filter(
+    (member) => member.category === 'Team Leader'
+  );
   // Get remaining team members (excluding CEO)
   const regularTeamMembers = teamMembers.filter(
-    (member) => member.position !== 'CEO'
+    (member) => member.category === 'Team Member'
   );
 
   // Handle card click
@@ -158,10 +215,32 @@ const AboutTeams = () => {
       {/* CEO Section */}
       {ceoData && <CEOSection ceo={ceoData} />}
 
-      {/* Employee Card Section - All members one by one */}
+      {/* Employee Card Section -  members one by one */}
       <div className="px-10 mt-16">
         <h2 className="text-2xl font-bold text-white mb-8 relative z-10 border-l-4 border-white pl-4">
-          Our Team Members
+          Team Leaders
+        </h2>
+
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:flex-row gap-10 flex-wrap justify-start items-center">
+          {teamLeader.map((member) => (
+            <TeamsCard
+              key={member.id}
+              name={member.name}
+              // email={member.email}
+              image={member.image}
+              position={member.position}
+              company={member.company}
+              isSelected={selectedMember === member.id}
+              onClick={() => handleCardClick(member.id)}
+            />
+          ))}
+        </ul>
+      </div>
+
+      {/* Employee Card Section -  members one by one */}
+      <div className="px-10 mt-16">
+        <h2 className="text-2xl font-bold text-white mb-8 relative z-10 border-l-4 border-white pl-4">
+          Team Members
         </h2>
 
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:flex-row gap-10 flex-wrap justify-start items-center">
